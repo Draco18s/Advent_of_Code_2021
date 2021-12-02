057b2c3ac8ab6a23a7a91cbdaaac2291e2ad8a28
Draco18s <draco18s@gmail.com>
2021-12-02 11:32:31 -0600

    Days 1 and 2

---
+++ Program.cs
@@ -0,0 +1,177 @@
+﻿using Advent_of_Code_2020.StatsBuilder;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2021 {
+	static class Program {
+		static void Main(string[] args) {
+			string input = File.ReadAllText(Path.GetFullPath("../../inputs/Day2.txt"));
+			input = input.Replace("\r", "");
+			//string input = @"";
+			DateTime s = DateTime.Now;
+			long result = DayTwo.Part1(input);
+			DateTime e = DateTime.Now;
+			Console.WriteLine(result);
+			Console.WriteLine("Time: " + (e - s).TotalMilliseconds);
+			s = DateTime.Now;
+			result = DayTwo.Part2(input);
+			e = DateTime.Now;
+			Console.WriteLine(result);
+			Console.WriteLine("Time: " + (e - s).TotalMilliseconds);
+			Console.ReadKey();
+			BuildLeaderboard();
+			Console.ReadKey();
+		}
+
+		static void BuildLeaderboard() {
+			string leaderboard = ""; //TODO: leaderboard json
+			Task.Run(async () => {
+				string input = await GetFromAsync(leaderboard);
+				AoCLeaderboard obj;
+				try {
+					obj = JsonSerializer.Deserialize<AoCLeaderboard>(input);
+				}
+				catch (Exception ex) {
+					Console.WriteLine(ex.Message);
+					return;
+				}
+				List<AoCUser> users = new List<AoCUser>();
+				foreach(AoCUser u in obj.members.Values) {
+					if(u.name == null) {
+						u.name = "(anonymous user #" + u.id + ")";
+					}
+					users.Add(u);
+				}
+				leaderboard = "";
+				input = await GetFromAsync(leaderboard);
+				obj = JsonSerializer.Deserialize<AoCLeaderboard>(input);
+				foreach(AoCUser u in obj.members.Values) {
+					if(u.name == null) {
+						u.name = "(anonymous user #" + u.id + ")";
+					}
+					if(users.Contains(u)) continue;
+					users.Add(u);
+				}
+
+				string mainTable = "<table> <tbody> <tr> <td class=\"typeheader\" colspan=\"3\">(25 items)<span class=\"fixedextenser\">4</span></td></tr><tr> <th title=\"System.String\">day</th> <th title=\"System.String,System.DateTime,System.Int32[]\">silver_order</th> <th title=\"System.String,System.DateTime,System.Int32[]\">gold_order</th> </tr>{0}</tbody></table>";
+				StringBuilder builder = new StringBuilder();
+				for(int d = 1; d <= 25; d++) {
+					if(Count(users, d.ToString(), "1") == 0) break;
+					builder.Append(GetTableRow(users, d));
+				}
+				builder.Append(GetTableRowScores(users));
+				input = File.ReadAllText(Path.GetFullPath("../../inputs/leaderboard_html.txt"));
+				input = input.Replace("{", "{{").Replace("}", "}}").Replace("{{0}}", "{0}");
+				if(File.Exists(Path.GetFullPath("../../leaderboard.html"))) {
+					File.Delete(Path.GetFullPath("../../leaderboard.html"));
+				}
+				File.WriteAllText(Path.GetFullPath("../../leaderboard.html"), string.Format(input, string.Format(mainTable, builder.ToString())));
+			});
+		}
+
+		private static async Task<string> GetFromAsync(string jsonurl) {
+			var baseAddress = new Uri("https://adventofcode.com");
+			var url = new Uri(baseAddress + jsonurl);
+			var cookieContainer = new CookieContainer();
+			cookieContainer.Add(baseAddress, new Cookie("session", "00000")); //TODO: Config file
+
+			HttpClient httpClient = new HttpClient(
+				new HttpClientHandler {
+					CookieContainer = cookieContainer,
+					AutomaticDecompression = DecompressionMethods.Deflate|DecompressionMethods.GZip,
+				}) {
+				BaseAddress = baseAddress,
+			};
+			var response = await httpClient.GetAsync(jsonurl);
+			if(response.IsSuccessStatusCode) {
+				return await response.Content.ReadAsStringAsync();
+			}
+			return string.Empty;
+		}
+
+		private static string GetTableRow(List<AoCUser> users, int d) {
+			string tablerowtable = "<tr> <td>{4}</td><td> <table> <tbody> <tr> <td class=\"typeheader\" colspan=\"3\">ø[{2}]&nbsp;<span class=\"fixedextenser\">4</span></td></tr><tr> <th title=\"System.String\">name</th> <th title=\"System.DateTime\">get_star_ts</th> <th title=\"System.Int32\">value</th> </tr>{0}</tbody> </table> </td><td> <table> <tbody> <tr> <td class=\"typeheader\" colspan=\"3\">ø[{3}]&nbsp;<span class=\"fixedextenser\">4</span></td></tr><tr> <th title=\"System.String\">name</th> <th title=\"System.DateTime\">get_star_ts</th> <th title=\"System.Int32\">value</th> </tr>{1}</tbody> </table> </td></tr>";
+			string day = d.ToString();
+			string[] parts = new string[2];
+			parts[0] = "";
+			parts[1] = "";
+			for(int p = 1; p <= 2; p++) {
+				string part = p.ToString();
+				SortUsers(ref users, day, part);
+				foreach(AoCUser user in users) {
+					int pts = GetPointsForUser(ref users, user.id, day, part);
+					if(d > 1)
+						user.locPoints += pts;
+					parts[p - 1] += GetUserLineScore(user, day, part, pts);
+				}
+			}
+			return string.Format(tablerowtable, parts[0], parts[1], Count(users, day, "1"), Count(users, day, "2"), day);
+		}
+
+		private static string GetTableRowScores(List<AoCUser> users) {
+			users.Sort((x, y) => y.locPoints.CompareTo(x.locPoints));
+			string tablerowtable = "<tr> <td>{2}</td><td> <table> <tbody> <tr> <td class=\"typeheader\" colspan=\"3\">ø[{1}]&nbsp;<span class=\"fixedextenser\">4</span></td></tr><tr> <th title=\"System.String\">name</th> <th title=\"System.Int32\">total</th> </tr>{0}</tbody> </table> </td> </table> </td></tr>";
+			string row = "";
+			foreach(AoCUser user in users) {
+				int pts = user.locPoints;
+				//user.locPoints += pts;
+				row += GetUserTotalScore(user, pts);
+			}
+			return string.Format(tablerowtable, row, users.Count, "Total");
+		}
+
+		private static int Count(List<AoCUser> users, string day, string part) {
+			return users.Count(x => {
+				return x.completion_day_level.ContainsKey(day) && x.completion_day_level[day].ContainsKey(part);
+			});
+		}
+
+		private static void SortUsers(ref List<AoCUser> users, string day, string part) {
+			users.Sort((x, y) => {
+				bool xhas = true;
+				bool yhas = true;
+				if(!y.completion_day_level.ContainsKey(day) || !y.completion_day_level[day].ContainsKey(part)) {
+					yhas = false;
+				}
+				if(!x.completion_day_level.ContainsKey(day) || !x.completion_day_level[day].ContainsKey(part)) {
+					xhas = false;
+				}
+				if(xhas && yhas)
+					return x.completion_day_level[day][part].dateTime.CompareTo(y.completion_day_level[day][part].dateTime);
+				else
+					return yhas.CompareTo(xhas);
+			});
+		}
+
+		private static int GetPointsForUser(ref List<AoCUser> users, string user, string day, string part) {
+			int i = users.FindIndex(x => x.id == user);
+			if(users[i].completion_day_level.ContainsKey(day) && users[i].completion_day_level[day].ContainsKey(part))
+				return users.Count - i;
+			return 0;
+		}
+
+		private static string GetUserLineScore(AoCUser user, string day, string part, int pts) {
+			string p = "<tr><td>{0}</td><td>{1}</td><td class=\"n\">{2}</td></tr>";
+			if(user.completion_day_level.ContainsKey(day) && user.completion_day_level[day].ContainsKey(part))
+				return string.Format(p, user.name, user.completion_day_level[day][part].dateTime.ToString("M/d/yyyy h:mm:ss tt"), pts);
+			long.TryParse(user.last_star_ts.ToString(), out long last);
+			if(last == 0)
+				return string.Empty;
+			return string.Format(p, user.name, "----", "0");
+		}
+
+		private static string GetUserTotalScore(AoCUser user, int pts) {
+			if(pts == 0) return string.Empty;
+			string p = "<tr><td>{0}</td><td class=\"n\">{1}</td></tr>";
+			return string.Format(p, user.name, pts.ToString());
+		}
+	}
+}
---
+++ StatsBuilder/AoCLeaderboard.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Advent_of_Code_2020.StatsBuilder {
+	public class AoCLeaderboard {
+		public string _event { get; set; }
+		public string owner_id { get; set; }
+		public Dictionary<string, AoCUser> members { get; set; }
+	}
+}
