c781b08b126d85ee72d63c0cc50be1776f441080
Draco18s <draco18s@gmail.com>
2021-12-10 15:13:58 -0500

    Day 9 and 10 Changed file names for better lexigraphical sorting.

---
+++ Day10.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent_of_Code_2021 {
+	internal static class DayTen {
+		internal static long Part1(string input) {
+			string[] lines = input.Split('\n');
+			long result = 0;
+			Dictionary<char,int> points = new Dictionary<char,int>();
+			points.Add(')',3);
+			points.Add(']',57);
+			points.Add('}',1197);
+			points.Add('>',25137);
+
+			foreach(string lin in lines) {
+				if(lin == string.Empty) continue;
+				Queue<char> output = new Queue<char>();
+				Stack<char> trainyard = new Stack<char>();
+				char last = ' ';
+				char c;
+				foreach(char exp in lin) {
+					if(exp == ' ') continue;
+					switch(exp) {
+						case '<':
+						case '{':
+						case '[':
+						case '(':
+							trainyard.Push(exp);
+							break;
+						case ']':
+							c = trainyard.Pop();
+							if(c == '[') break;
+							last = exp;
+							goto invalidFound;
+						case '}':
+							c = trainyard.Pop();
+							if(c == '{') break;
+							last = exp;
+							goto invalidFound;
+						case '>':
+							c = trainyard.Pop();
+							if(c == '<') break;
+							last = exp;
+							goto invalidFound;
+						case ')':
+							c = trainyard.Pop();
+							if(c == '(') break;
+							last = exp;
+							goto invalidFound;
+						default:
+							break;
+					}
+				}
+				continue;
+				invalidFound:
+				if(last != ' ') {
+					result += points[last];
+				}
+			}
+			return result;
+		}
+
+		internal static long Part2(string input) {
+			string[] lines = input.Split('\n');
+			long result = 1;
+			Dictionary<char,int> points = new Dictionary<char,int>();
+			points.Add(')',1);
+			points.Add(']',2);
+			points.Add('}',3);
+			points.Add('>',4);
+			Dictionary<char,char> lookup = new Dictionary<char,char>();
+			lookup.Add('(',')');
+			lookup.Add('[',']');
+			lookup.Add('{','}');
+			lookup.Add('<','>');
+			List<long> scores = new List<long>();
+			foreach(string lin in lines) {
+				if(lin == string.Empty) continue;
+				Queue<char> output = new Queue<char>();
+				Stack<char> trainyard = new Stack<char>();
+				char last = ' ';
+				char c;
+				foreach(char exp in lin) {
+					if(exp == ' ') continue;
+					switch(exp) {
+						case '<':
+						case '{':
+						case '[':
+						case '(':
+							trainyard.Push(exp);
+							break;
+						case ']':
+							c = trainyard.Pop();
+							if(c == '[') break;
+							last = exp;
+							goto invalidFound;
+						case '}':
+							c = trainyard.Pop();
+							if(c == '{') break;
+							last = exp;
+							goto invalidFound;
+						case '>':
+							c = trainyard.Pop();
+							if(c == '<') break;
+							last = exp;
+							goto invalidFound;
+						case ')':
+							c = trainyard.Pop();
+							if(c == '(') break;
+							last = exp;
+							goto invalidFound;
+						default:
+							break;
+					}
+				}
+				string append = "";
+				foreach(char c2 in trainyard) {
+					append += lookup[c2];
+				}
+				long score = 0;
+				foreach(char c2 in append) {
+					score *= 5;
+					score += points[c2];
+				}
+				scores.Add(score);
+				continue;
+				invalidFound:
+				continue;
+			}
+			scores.Sort();
+			int ind = (scores.Count / 2);
+			return scores[ind];
+		}
+	}
+}
---
+++ Day9.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent_of_Code_2021 {
+	internal static class DayNine {
+		internal static long Part1(string input) {
+			string[] lines = input.Split('\n');
+			long result = 0;
+			int[,] grid = new int[lines[0].Length,lines.Length];
+			{
+				int y = 0;
+				foreach(string lin in lines) {
+					int x = 0;
+					char[] ca = lin.ToCharArray();
+					foreach(char c in ca) {
+						grid[x,y] = int.Parse(c.ToString());
+						x++;
+					}
+
+					y++;
+				}
+			}
+			{
+				for(int y = 0; y < lines.Length; y++) {
+					for(int x = 0; x < lines[0].Length; x++) {
+						bool isLow = GetLow(x, y, grid, lines[0].Length, lines.Length);
+						result += isLow ? 1 + grid[x,y] : 0;
+					}
+				}
+			}
+			return result;
+		}
+
+		private static bool GetLow(int x, int y, int[,] grid, int mx, int my) {
+			int N = (y == 0)?99:grid[x,y-1];
+			int W = (x == 0)?99:grid[x-1,y];
+
+			int S = (y == my-1)?99:grid[x,y+1];
+			int E = (x == mx-1)?99:grid[x+1,y];
+
+			int L = grid[x,y];
+
+			return (L < N && L < S && L < E && L < W);
+		}
+
+		internal static long Part2(string input) {
+			string[] lines = input.Split('\n');
+			long result = 1;
+			int[,] grid = new int[lines[0].Length,lines.Length];
+			{
+				int y = 0;
+				foreach(string lin in lines) {
+					int x = 0;
+					char[] ca = lin.ToCharArray();
+					foreach(char c in ca) {
+						grid[x,y] = int.Parse(c.ToString());
+						x++;
+					}
+
+					y++;
+				}
+			}
+			List<Vec2> lows = new List<Vec2>();
+			{
+				for(int y = 0; y < lines.Length; y++) {
+					for(int x = 0; x < lines[0].Length; x++) {
+						bool isLow = GetLow(x, y, grid, lines[0].Length, lines.Length);
+						//result += isLow ? 1 + grid[x,y] : 0;
+						if(isLow) lows.Add(new Vec2() { x= x, y= y});
+					}
+				}
+			}
+			List<long> sizes = new List<long>();
+			foreach(Vec2 p in lows) {
+				long a = FindBasin(p, grid, lines[0].Length, lines.Length);
+				sizes.Add(a);
+			}
+			sizes.Sort();
+			int mm = sizes.Count-1;
+			return sizes[mm-0] * sizes[mm-1] * sizes[mm-2];
+		}
+		
+		private static long FindBasin(Vec2 lowPoint, int[,] grid, int mx, int my) {
+			long size = 1;
+			int x = lowPoint.x;
+			int y = lowPoint.y;
+			if(x >= mx) return 0;
+			if(y >= my) return 0;
+			int L = grid[x,y];
+			if(L == 9) return 0;
+
+			int N = (y == 0)?-99:grid[x,y-1];
+			int W = (x == 0)?-99:grid[x-1,y];
+
+			int S = (y == my-1)?99:grid[x,y+1];
+			int E = (x == mx-1)?99:grid[x+1,y];
+			grid[x,y] = 9;
+			if(N >= L) {
+				size += FindBasin(new Vec2(){x= x, y= y-1}, grid, mx, my);
+			}
+			if(S >= L) {
+				size += FindBasin(new Vec2(){x= x, y= y+1}, grid, mx, my);
+			}
+			if(W >= L) {
+				size += FindBasin(new Vec2(){x= x-1, y= y}, grid, mx, my);
+			}
+			if(E >= L) {
+				size += FindBasin(new Vec2(){x= x+1, y= y}, grid, mx, my);
+			}
+			return size;
+		}
+
+		public struct Vec2 {
+			public int x;
+			public int y;
+		}
+	}
+}
