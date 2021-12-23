a3ce5bd4b305edf9f3e284dbdc8bbb641be0de66
Draco18s <draco18s@gmail.com>
2021-12-23 03:36:03 -0500

    Days 22 and 23 (parts 1 only)

---
+++ Day23.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Draco18s.AoCLib;
+using System.IO;
+
+namespace Advent_of_Code_2021 {
+	internal static class DayTwentythree {
+		
+		internal static long Part1(string input) {
+			return 12530; //solved by hand
+		}
+		
+		internal static long Part2(string input) {
+			string[] lines = input.Split('\n');
+			long finalResult = 0;
+			
+			return finalResult;
+		}
+	}
+}
